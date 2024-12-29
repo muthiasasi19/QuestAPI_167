b1d2367fad80b89a604768c73fff2cb24ab13694
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-29 19:53:49 +0700

    Membuat fungsi untuk mengambil dan menghapus data

--- app/src/main/java/com/example/pertemuan_12/ui/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan_12/ui/viewModel/HomeViewModel.kt
@@ -1,4 +1,5 @@
 package com.example.pertemuan_12.ui.viewModel
+
 import android.net.http.HttpException
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,5 +23,31 @@ class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
         private set
 
+    init {
+        getMhs()
+    }
 
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+    fun deleteMhs(nim : String){
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            }catch (e : IOException){
+                HomeUiState.Error
+            }catch (e : HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
 }
