bc5e7da7097b14f3116c77ec14d3b7a0877a0029
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-12-29 14:09:22 +0700

    Membuat fungsi konversi data antara UI dan model Mahasiswa menggunakan data class dan extension

---
+++ app/src/main/java/com/example/pertemuan_12/ui/viewModel/InsertViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.pertemuan_12.ui.viewModel
+
+
+import com.example.pertemuan_12.model.Mahasiswa
+
+
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+fun InsertUiEventtoMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
